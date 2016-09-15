399a48000b0ca41f02889d714307637286e83a04
printer83mph <printer.83mph@gmail.com>
2016-09-14 21:30:13 -0400

    Add all existing files

---
+++ README.md
@@ -0,0 +1,30 @@
+# MAPS
+
+Mostly Accurate Physics Simulation
+By Thomas Shaw
+
+## Instructions
+### How do I start???
+
+Open index.html. Double clicking it should work, but if it doesn't open with whatever browser you want to use. Remember that this is a browser based program, so refreshing the page should fix any issues that pop up.
+
+### How do I do things???
+
+*Note: If any of these don't work, click inside the browser window to make sure your keyboard is recognized.*
+  
+* Use F to create a ball of random mass at your mouse location. Don't worry, they all have the same density, so you can tell their mass by their size.
+* Press R over a ball to remove it, and C to remove all balls from the screen.
+* Click and hold on a ball to grab hold of it, the force applied on it is relative to the distance from the center of it to your mouse.
+  
+### What do the lines mean???
+
+* Red is acceleration, so if you're not dragging the ball, it'll just be some downwards force from gravity.
+* Blue is velocity, this will change depending on the speed and direction of the ball.
+* Black is the grabber line from your mouse to the ball, the longer it is the harder it pulls the ball.
+
+**## Credits
+
+Code: Thomas Shaw
+Idea: Thomas Shaw
+
+**
---
+++ sketch.js
@@ -0,0 +1,96 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  smooth();
+  strokeWeight(5);
+  frameRate(60);
+}
+
+var minRadius = 10;
+var maxRadius = 100;
+
+var balls = [];
+
+var mouseP = new p5.Vector(0,0);
+
+function Ball(x,y,r) {
+  this.position = new p5.Vector(x,y);
+  this.velocity = new p5.Vector(0,0);
+  this.acceleration = new p5.Vector(0,0);
+  this.r = r;
+  this.beingDragged = false;
+  this.checkEdgeCollide = function() {
+    if (this.position.x < this.r) {
+      this.position.x = this.r;
+      this.velocity.x *= -0.8;
+    } if (this.position.x > width-this.r) {
+      this.position.x = width-this.r;
+      this.velocity.x *= -0.8;
+    } if (this.position.y < this.r) {
+      this.position.y = this.r;
+      this.velocity.y *= -0.8;
+    } if (this.position.y > height-this.r) {
+      this.position.y = height-this.r;
+      this.velocity.y *= -0.8;
+    }
+  }
+  this.draw = function() {
+    fill(0,0,0,0);
+    stroke(0);
+    ellipse(this.position.x,this.position.y,this.r*2,this.r*2); // draw ellipse
+    stroke(0,0,255,120);
+    line(this.position.x,this.position.y,this.position.x+this.velocity.x*5,this.position.y+this.velocity.y*5);
+    stroke(255,0,0,120);
+    line(this.position.x,this.position.y,this.position.x+this.acceleration.x*100,this.position.y+this.acceleration.y*100);
+    this.acceleration.set(0,0.3)
+    this.checkEdgeCollide(); // check for collisions
+    if(this.beingDragged) {
+      this.acceleration.add((mouseP.x-this.position.x)/5/this.r,(mouseP.y-this.position.y)/5/this.r);
+      stroke(0,120);
+      line(this.position.x,this.position.y,mouseX,mouseY);
+    }
+    this.velocity.add(this.acceleration);
+    this.velocity.mult(0.995); // air friction
+    this.position.add(this.velocity); // add velocity to pos
+    if(this.position.y > height-this.r) {
+      this.velocity.x *= 0.99;
+    } // ground friction
+  }
+}
+
+function mousePressed() {
+  for(var i in balls) {
+    if(dist(balls[i].position.x,balls[i].position.y,mouseX,mouseY) < balls[i].r) {
+      balls[i].beingDragged = true;
+    }
+  }
+}
+function mouseReleased() {
+  for(var i in balls) {
+    balls[i].beingDragged = false;
+  }
+}
+
+function draw() {
+  mouseP.set(mouseX,mouseY);
+  background(255,255,255);
+  for(var i in balls) {
+    balls[i].draw();
+  }
+}
+
+function keyPressed() {
+  if(key === "F") {
+    balls.push(new Ball(mouseX,mouseY,random(minRadius,maxRadius)))
+  }
+  if(key === "C") {
+    balls = [];
+  }
+  if(key === "R") {
+    for(var i in balls) {
+      if(dist(balls[i].position.x,balls[i].position.y,mouseX,mouseY) < balls[i].r) {
+        balls.splice(i,1);
+        i++;
+      }
+    }
+  }
+}
